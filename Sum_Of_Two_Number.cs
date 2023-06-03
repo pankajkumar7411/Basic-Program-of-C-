ae892054fd03589709cda239f261ddab3a8fcb9f
PANKAJ KUMAR <132256534+pankajkumar7411@users.noreply.github.com>
2023-06-03 19:26:28 +0530

    Add files via upload

---
+++ Sum_Of_Two_Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pankaj
+{
+    internal class Sum_Of_Two_Number
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of two number is:" + sum);
+            Console.ReadLine();
+        }
+    }
+}

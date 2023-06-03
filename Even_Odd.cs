8efc04a94942ff0bceced81430ca370db4ec2842
PANKAJ KUMAR <132256534+pankajkumar7411@users.noreply.github.com>
2023-06-03 19:27:53 +0530

    Add files via upload

---
+++ Even_Odd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pankaj
+{
+    internal class Even_Odd
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x % 2 ==0)
+            {
+                Console.WriteLine("The number is Even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+            Console.ReadLine();
+        }
+    }
+}

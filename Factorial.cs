ff54d5e70824f9c6a8b7d4f210113c22d0456b1c
PANKAJ KUMAR <132256534+pankajkumar7411@users.noreply.github.com>
2023-06-05 22:02:29 +0530

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pankaj
+{
+    internal class Factorial
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number:");
+            int x = int.Parse(Console.ReadLine());
+            int fact =1;
+            for(int i=1; i<=x; i++)
+                {
+                fact = fact * i;
+            }
+            Console.WriteLine("The factorial of "+x+" is: "+fact);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FibonacciSerise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pankaj
+{
+    internal class FibonacciSerise
+    {
+        static void Main()
+        {
+            int num1 = 1, num2 = 2, num3, number;
+            Console.WriteLine("Enter the number of Elements: ");
+            number = int.Parse(Console.ReadLine());
+            for(int i =2;i<number;i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
